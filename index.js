099d3f94728fee4a8dc61ff142f7512bd04710cf
TrueReassembly <62158683+TrueReassembly@users.noreply.github.com>
2025-08-20 20:24:19 +0100

    aaaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { WebSocket } = require("ws")
 
-const wss = new WebSocket('wss://scrapyard.reassembly.dev/ws')
+const wss = new WebSocket('wss://qs4k0cc4gokwgkwcsoogo8ks.51.195.138.100.sslip.io/ws')
 
 wss.on('open', () => {
     console.log("WebSocket connection established")
